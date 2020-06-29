11bf53b21d8b7ebea065afa60cdbf2f658c718f3
CameronChildress <cameronchildress4@gmail.com>
2020-06-29 16:34:42 -0600

    Added Core::

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -1,9 +1,27 @@
 // Game.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#include "core.h"
 #include <iostream>
+
+bool Update(float dt) 
+{ 
+    return false; 
+}
+
+void Draw(Core::Graphics& graphics)
+{
+    graphics.SetColor(RGB(rand() % 256, rand() % 256, rand() % 256)); 
+    graphics.DrawLine(static_cast<float>(rand() % 800), static_cast<float>(rand() % 600), 
+        static_cast<float>(rand() % 800), static_cast<float>(rand() % 600));
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    char name[] = "ChildressC"; 
+    Core::Init(name, 800, 600);
+    Core::RegisterUpdateFn(Update);
+    Core::DrawFn(Draw);
+    Core::GameLoop(); 
+    Core::Shutdown();
 }
